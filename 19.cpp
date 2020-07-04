de9207c96afae0c760a7d0a59321562f8b1ebd57
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2020-07-04 10:31:44 +0530

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--)
+    {
+        for (int space = 0; space < n - i; space++)
+            cout << " ";
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
+            if(j==0 || j==2*i-2 || i==n)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
